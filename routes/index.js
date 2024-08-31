1ba4adf466f5e56cf1975a6a029a3bacf27460d5
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-01 02:42:17 +0300

    refactor: controller to add new user

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 // Utility routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// User creation route
+router.post('/users', UsersController.postNew);
 
 export default router;
