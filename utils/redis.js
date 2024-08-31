ed8297f7541f6b91d8222f61a6694b2de4f77875
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-01 01:44:02 +0300

    refactor: add error checking in all async methods

--- utils/redis.js
+++ utils/redis.js
@@ -3,13 +3,20 @@ import { promisify } from 'util';
 
 class RedisClient {
     constructor() {
+	this.connected = true;
+
         // Create a Redis client
         this.client = redis.createClient();
 
         // Display error in console
         this.client.on('error', (error) => {
-            console.error(`Redis client not connected to the server: ${error.message}`)
+            console.error(`Redis client not connected to the server: ${error.message}`);
+	    this.connected = false;
         });
+
+	this.client.on('connect', () => {
+	   this.connected = true;
+	});
 
         // Promisify Redis methods for asynchronous usage
         this.getAsync = promisify(this.client.get).bind(this.client);
@@ -19,11 +26,12 @@ class RedisClient {
 
     // Returns true if the connection to Redis is successful, otherwise false
     isAlive() {
-        return this.client.connected;
+        return this.connected;
     }
 
     // Asynchronous function to get the value of a key in Redis
     async get(key){
+	if (!this.isAlive()) return undefined;
         try {
             const value = await this.getAsync(key);
             return value;
@@ -35,8 +43,9 @@ class RedisClient {
 
     // Asynchronous function to set a key-value pair in Redis with an expiration
     async set(key, value, duration) {
+	if (!this.isAlive()) return;
         try {
-            await this.setAsync(key, value, 'EX', duration)
+            await this.setAsync(key, value, 'EX', duration);
         } catch (error) {
             console.error(`Error setting key ${key}: ${error.message}`);
         }
@@ -44,8 +53,9 @@ class RedisClient {
 
     // Asynchronous function to delete a key in Redis
     async del(key) {
+	if (!this.isAlive()) return;
         try {
-            await this.delAsync(key)
+            await this.delAsync(key);
         } catch {
             console.log(`Error deleting key ${key}: ${error.message}`);
         }
