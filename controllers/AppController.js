71c21e7a8fe67df525712923fbb19ccb4b0651bf
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-01 01:34:55 +0300

    feat: add route controllers

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+
+  // Controller for the /status endpoint
+  static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+    return res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
+
+  // Controller for the /stats endpoint
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    return res.status(200).json({ users: usersCount, files: filesCount });
+  }
+}
+
+export default AppController;
