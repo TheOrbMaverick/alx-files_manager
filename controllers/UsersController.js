b9519d3d626dc13f362294e5c487af2e3b771c8e
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-02 02:17:53 +0300

    feat: create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,13 @@
 import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+const userQueue = new Queue('sending an email');
+
 async function postNew(req, res) {
-  const { email } = req.body;
-  const { password } = req.body;
+  const email = req.body ? req.body.email : null;
+  const password = req.body ? req.body.password : null;
 
   if (email === undefined) {
     return res.status(400).json({ error: 'Missing email' });
@@ -15,15 +18,23 @@ async function postNew(req, res) {
   }
 
   try {
-    const existingUser = await dbClient.findUser(email);
-    if (existingUser) {                                               return res.status(400).json({ error: 'Already exist' });
+    const existingUser = await (await dbClient.usersCollection()).findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
     }
 
     const hashedPassword = sha1(password);
-    const _id = await dbClient.saveUser(email, password);
 
-    res.status(201).json({ id: _id, email });
-  } catch {
+    const insertInfo = await (
+      await dbClient.usersCollection()
+    ).insertOne( { email, hashedPassword });
+
+    const id = insertInfo.insertedId.toString();
+    userQueue.add({ id });
+
+    res.status(201).json({ id: id, email });
+  } catch(error) {
+    console.error(error);
     res.status(500).json({ error: 'Internal Server error' });
   }
 }
