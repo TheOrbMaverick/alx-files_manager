1edb78addf3f72829606c608e22743e1dd748d90
TheOrbMaverick <enangita@gmail.com>
2024-09-02 16:25:04 +0100

    usercontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,5 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1'; // Import the SHA1 hashing library
+import dbClient from '../utils/db';
 
 class UsersController {
   // Handler for the POST /users endpoint
