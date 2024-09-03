383944053deb0d03e1dcb3cc5b480506a6a5d689
TheOrbMaverick <enangita@gmail.com>
2024-09-03 13:59:35 +0100

    change2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/', routes);
 
 app.listen(port, (err) => {
   if (err) { console.log(err); }
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
