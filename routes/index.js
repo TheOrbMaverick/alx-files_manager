56079d52b8d506793fed239f7626168764b80fc6
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-04 17:48:43 +0300

    feat: add two new endpoints

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,7 @@ router.get('/users/me', UsersController.getMe);
 // File controller routes
 router.post('/files', FilesController.postUpload);
 
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
 export default router;
